c8f23fa8c86f2b65356e056b5711e972f0e28947
Kevin <kevin0tran@gmail.com>
2024-04-18 22:59:00 -0400

    Documenting API request script and clean up

--- data_retrieval/api_retreiver.py
+++ data_retrieval/api_retreiver.py
@@ -1,11 +1,13 @@
-import requests
-import time
+"""Using the pokeapi.co to pull information and sprites"""
 import json
+import time
+import requests
 
 pokemon = {}
 i = 1
 while i < 101:
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i}/")
+    response = requests.get(
+        f"https://pokeapi.co/api/v2/pokemon/{i}/", timeout=10)
 
     file = response.json()
     individual = {"national_dex": i, "name": file['name'], "type": file['types'],
